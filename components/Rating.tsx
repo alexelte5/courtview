8c35315d47463367a5e263f37fc2732f2cd0919b
alexelte5 <alexeltekow@gmail.com>
2025-11-07 09:10:59 +0100

    added chakraUI

---
+++ components/Rating.tsx
@@ -0,0 +1,7 @@
+export default function Rating() {
+    return (
+        <div>
+            Hier ist rating
+        </div>
+    )
+}
---
+++ components/Serivce.tsx
@@ -0,0 +1,7 @@
+export default function Service() {
+    return (
+        <div>
+            Hier ist Service
+        </div>
+    )
+}
